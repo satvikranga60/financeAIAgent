db900842f5aba17300f89bf71ab3bdf3b1c5a962
Satvik Ranga <satvikranga60@gmail.com>
2025-01-30 16:04:43 -0500

    Add files via upload

---
+++ finance_agent.py
@@ -0,0 +1,18 @@
+from phi.agent import Agent
+from phi.model.groq import Groq
+from phi.tools.yfinance import YFinanceTools
+from dotenv import load_dotenv
+
+load_dotenv()
+   
+
+agent = Agent(
+    model=Groq(id="llama-3.3-70b-versatile"),
+    tools= [YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True)],
+    show_tool_calls=True,
+    markdown=True,
+    instructions=["Use tables to display data. "]
+    
+)
+
+agent.print_response("Summarize and compare analyst recommendations and fundamentals for TSLA and NVDA")
